d631be34f6ef8929917986e2fd0fdae5694e91cd
ArminJaemaa <ajaemaa@tlu.ee>
2025-03-26 17:01:31 +0200

    Uus kell. Näitab õiget aega ja liigub õigesti.

--- 1Kodutöö/clock.js
+++ 1Kodutöö/clock.js
@@ -1,3 +1,5 @@
+console.log("Js töötab");
+
 function updateClock() {
     const now = new Date();
 
@@ -6,16 +8,16 @@ function updateClock() {
     const seconds = now.getSeconds();
 
     const hourDegree = (hours % 12) * 30 + (minutes / 2);
-    const minuteDegree = minutes * 6;
+    const minuteDegree = minutes * 6 + (seconds * 0.1);
     const secondDegree = seconds * 6;
 
     const hourHand = document.querySelector(".hour");
     const minuteHand = document.querySelector(".minute");
     const secondHand = document.querySelector(".second");
 
-    hourHand.style.transform = `rotate(${90 + hourDegree}deg)`;
-    minuteHand.style.transform = `rotate(${90 + minuteDegree}deg)`;
-    secondHand.style.transform = `rotate(${90 + secondDegree}deg)`;
+    hourHand.style.transform = `rotate(${hourDegree}deg)`;
+    minuteHand.style.transform = `rotate(${minuteDegree}deg)`;
+    secondHand.style.transform = `rotate(${secondDegree}deg)`;
 }
 
 setInterval(updateClock, 1000);
